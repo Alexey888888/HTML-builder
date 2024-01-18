edc7744440b6a48d2108f2e40798dea1be1f4540
Alexey888888 <brdkmail@gmail.com>
2024-01-18 17:16:26 +0200

    feat: implement 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const stdout = process.stdout;
+const stdin = process.stdin;
+const exit = process.exit;
+
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const rl = readline.createInterface(stdin, stdout);
+const writableStream = fs.createWriteStream(path.join(__dirname, 'text.txt'), {
+  flags: 'a',
+});
+
+stdout.write('Hi! Input your text:\n');
+
+rl.on('line', (data) => {
+  if (data === 'exit') exit();
+  writableStream.write(`${data}\n`);
+});
+
+process.on('exit', () => {
+  stdout.write('Bye! Have a nice day!');
+});
